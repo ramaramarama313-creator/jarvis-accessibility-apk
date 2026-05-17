d00ac97009851680767f36c7a0871fab88a3506a
ramaramarama313-creator <ramaramarama313@gmail.com>
2026-05-17 17:35:34 +0530

    Create app/src/main/java/com/jarvis/automation/MainActivity.java

--- app/src/main/java/com/jarvis/automation/MainActivity.java
+++ app/src/main/java/com/jarvis/automation/MainActivity.java
@@ -3,12 +3,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 public class MainActivity extends Activity {
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
+@Override protected void onCreate(Bundle s) { super.onCreate(s);
 TextView tv = new TextView(this);
-tv.setText("जार्विस इंजन एक्टिव है!");
-tv.setTextSize(20);
-setContentView(tv);
-}
-}
+tv.setText("जार्विस न्यूरल लिंक एक्टिव है! टर्मक्स से कनेक्टेड।");
+setContentView(tv); } }
